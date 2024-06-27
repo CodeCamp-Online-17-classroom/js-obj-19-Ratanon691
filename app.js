ab50c3085d06a46b3a28cb6b650b55508081f317
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-27 16:02:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+var name = 'Joe';
+function makeUser() {
+  return {
+    name: 'John',
+    ref: this
+  };
+}
+let user = makeUser();
+console.log(user.ref.name); // Joe
+//เพราะJoeเป็นglobal variable และ ref: this ตัวนี้จะชี้ไปที่global variable เพราะว่าfunctionนี้ไม่ใช่medthodของObj ซึ่ง global variable มี name: `Joe` ทำให้user.ref.name ชี้ไปที่ Joe
